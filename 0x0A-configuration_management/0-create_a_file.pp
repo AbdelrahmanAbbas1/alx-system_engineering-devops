86c7c036470f7c723233c45a798c4b78868fd1ae
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-01-14 18:32:56 +0000

    Adding new file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 file { '/tmp/school':
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
   content => 'I love puppet'
 }
